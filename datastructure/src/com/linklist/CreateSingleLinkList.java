5da468426c54f9837585c86d2888df1b9dcd2617
sujeetk1 <ksujeet2912@gmail.com>
2018-09-23 12:14:15 +0530

    Creating and Traversing

--- datastructure/src/com/linklist/CreateSingleLinkList.java
+++ datastructure/src/com/linklist/CreateSingleLinkList.java
@@ -1,10 +1,44 @@
 package com.linklist;
 
 public class CreateSingleLinkList {
+	
+	private Node head;
+	
+	static class Node{
+		int value;
+		Node next;
+		
+		Node(int value, Node next){
+			this.value = value;
+			this.next = next;
+		}
+		
+		@Override
+		public String toString() {
+			return "Value ::"+this.value;
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		CreateSingleLinkList singleLL = new CreateSingleLinkList();
+		
+		singleLL.head = new Node(1, null);
+		
+		Node n1 = new Node(2, null);
+		
+		singleLL.head.next = n1;
+		
+		print(singleLL.head);
+	}
+	
+	private static void print(Node head){
+		
+		while(head != null){
+			System.out.println(head.value);
+			head = head.next;
+		}
+		
 	}
 
 }
